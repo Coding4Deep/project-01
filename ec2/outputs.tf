aef95975746423bc68cc576503049f728eae0cc2
Coding4Deep <sagardeepak2002@gmail.com>
2025-10-15 17:31:47 +0530

    adding ansible code

--- ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ec2_ips" {
-  value = { for k, v in aws_instance.servers : k => v.public_ip }
-}
--- ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ec2_instances" {
-  description = "Map of EC2 instance configurations"
-  type = map(object({
-    ami           = string
-    instance_type = string
-    subnet_id     = string
-  }))
-}
