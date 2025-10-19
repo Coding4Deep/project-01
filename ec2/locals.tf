aa7b92521bae6be18c42f0eecec64ecbf26f4d42
Coding4Deep <sagardeepak2002@gmail.com>
2025-10-19 16:15:06 +0530

    adding jenkinsfile

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,10 +1,10 @@
 locals {
   ec2_configs = {
-    jen-master-02 = {
-      ami           = "ami-0360c520857e3138f"
-      instance_type = "t2.medium"
-      subnet_id     = module.vpc.public_subnet_id
-    }  
+    # jen-master-02 = {
+    #   ami           = "ami-0360c520857e3138f"
+    #   instance_type = "t2.medium"
+    #   subnet_id     = module.vpc.public_subnet_id
+    # }  
 
     # jenkins-master-02 = {
     #   ami           = "ami-0360c520857e3138f"
