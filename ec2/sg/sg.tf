de62a8f413b60a7baa7cb66dbc6fb6b9664c7d0f
Coding4Deep <sagardeepak2002@gmail.com>
2025-10-22 15:37:20 +0530

    modified

--- ec2/sg/sg.tf
+++ ec2/sg/sg.tf
@@ -1,10 +1,9 @@
-
 resource "aws_security_group" "ec2_sg" {
   name        = var.sg_name
   description = "EC2 Security Group with dynamic ingress"
   vpc_id      = var.vpc_id
 
-  #  Dynamic ingress rules
+  # 🔹 Dynamic ingress rules for TCP ports
   dynamic "ingress" {
     for_each = var.allowed_ports
     content {
@@ -15,20 +14,17 @@ resource "aws_security_group" "ec2_sg" {
       description = "Allow TCP port ${ingress.value}"
     }
   }
-  ingress = [
-    {
-      from_port   = -1
-      to_port     = -1
-      protocol    = "icmp"
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      description      = "Allow ping from anywhere"
-    }
-  ]
 
-  # Allow all egress traffic
+  # 🔹 Static ingress rule for ICMP (ping)
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ping from anywhere"
+  }
+
+  # 🔹 Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
