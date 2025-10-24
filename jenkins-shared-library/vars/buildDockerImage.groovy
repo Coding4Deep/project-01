e3f07756f56527dfea5eaadef70ecbd45239524a
Coding4Deep <sagardeepak2002@gmail.com>
2025-10-24 18:33:04 +0530

    addin all jenkins file

--- jenkins-shared-library/vars/buildDockerImage.groovy
+++
@@ -1,4 +0,0 @@
-def call(Map config) {
-    echo "Building Docker image for ${config.serviceName}..."
-    sh "docker build -t ${config.serviceName}:${config.imageTag} ."
-}
--- jenkins-shared-library/vars/pushToECR.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map config) {
-    withAWS(credentials: 'awscreds', region: "${config.awsRegion}") {
-        sh """
-            aws ecr get-login-password --region ${config.awsRegion} | docker login --username AWS --password-stdin ${config.ecrAccountId}.dkr.ecr.${config.awsRegion}.amazonaws.com
-            docker tag ${config.serviceName}:${config.imageTag} ${config.ecrAccountId}.dkr.ecr.${config.awsRegion}.amazonaws.com/${config.ecrRepoName}:${config.imageTag}
-            docker push ${config.ecrAccountId}.dkr.ecr.${config.awsRegion}.amazonaws.com/${config.ecrRepoName}:${config.imageTag}
-        """
-    }
-}
